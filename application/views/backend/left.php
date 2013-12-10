d37112a4a5f9850ffadd9eb27b2c62670affabe3
masterit92 <thebinh1992@gmail.com>
2013-12-10 10:46:11 +0700

    binhok

--- application/views/backend/left.php
+++ application/views/backend/left.php
@@ -4,7 +4,7 @@
     <div class="container-menu-content">
         <div id='cssmenu'>
             <ul>
-                <li class='active '><a href='#'><span>Home</span></a></li>
+                <li class='active'><a href='#'><span>Home</span></a></li>
                 <li><a href='#'><span>User</span></a></li>
                 <li><a href='#'><span>Category</span></a></li>
                 <li><a href='#'><span>Product</span></a></li>
